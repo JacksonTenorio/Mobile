16dbb0b4da2dcf55f823712678851ed023bd296c
Jackson <jacksontenorio8@gmail.com>
2020-10-20 18:10:44 -0300

    kotlin

---
+++ variavel.kt
@@ -0,0 +1,11 @@
+fun main ( args : Array < String >) {
+    // Ao utilizar var, é criado uma variável mutavel.
+    var usuario = 'Jackson Tenorio'
+    usuario = 'Domingos'
+    // Ao utilizar val, é criado uma variável imutavel.
+    val pi = 3.14
+    var salario = 100
+    var bonus = 20
+    var total = salario + bonus
+    println ("O salario é $ total. ")
+}
