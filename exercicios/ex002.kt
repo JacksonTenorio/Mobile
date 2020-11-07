4e963ea875af1a0305ddae9bc325d27472dbdd52
Jackson <jacksontenorio8@gmail.com>
2020-11-06 21:36:16 -0300

    Update ex002.kt

--- exercicios/ex002.kt
+++ exercicios/ex002.kt
@@ -1,4 +1,4 @@
-// Crie funções para as 4 operações aritméticas básicas.
+// 1.Crie funções para as 4 operações aritméticas básicas.
 fun somar(valor1: int, valor2:int){
     println(valor1 + valor2)
 }
@@ -15,3 +15,12 @@ somar(1, 1)
 subtrair(2, 4)
 multiplicar(10, 2)
 dividir(10, 2)
+/* 2.Utilizando a estrutura condicional, desenvolva um algoritmo que determine se uma pessoa é maior ou menor de idade. */
+fun main(){
+    val idade = 33
+    if(idade >= 18){
+        println("Você é de maior idade")
+    }else{
+        println("Você é de menor idade")
+    }
+}
