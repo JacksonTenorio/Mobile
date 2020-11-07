03af243f38bf194a65527fbfe82134f803ad17c3
Jackson <jacksontenorio8@gmail.com>
2020-11-06 21:06:30 -0300

    Update ex002.kt

--- exercicios/ex002.kt
+++ exercicios/ex002.kt
@@ -1,1 +1,17 @@
 // Crie funções para as 4 operações aritméticas básicas.
+fun somar(valor1: int, valor2:int){
+    println(valor1 + valor2)
+}
+fun subtrair(valor: int - valor2){
+    println(valor1 - valor2)
+}
+fun multiplicar(valor1 * valor2){
+    println(valor1 * valor2)
+}
+fun dividir(valor1 / valor2){
+    println(valor1 / valor2)
+}
+somar(1, 1)
+subtrair(2, 4)
+multiplicar(10, 2)
+dividir(10, 2)
